48ce4f9b2e24a29f917bb9a6732eae83da235da2
Carlos Tavares <cast.3@hotmail.com>
2019-09-12 11:18:13 -0300

    Estruturas de repetição

---
+++ EstruturaDeRepeticao/Atividade1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            for(int i = 0; i < 10; i = i + 2)
+            {
+                Console.WriteLine("Informe seu nome: ");
+                nome = Console.ReadLine();
+            }
+
+            Console.WriteLine("Testando meu exe");
+            Console.ReadKey();
+        }
+    }
+}
