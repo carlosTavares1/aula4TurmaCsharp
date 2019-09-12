4c4b4d4bf5a7431bec140765b45a0cff20754a9b
Carlos Tavares <cast.3@hotmail.com>
2019-09-12 16:52:31 -0300

    Estrutura de repetição while e for procurando palavras.

---
+++ LacosDeRepeticao/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int nome;
+
+
+            private
+            public
+            protected
+            default
+        }
+    }
+}
